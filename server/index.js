aa31425472a3a4376a360c5db39b53b45fab7cee
Zygrade <hsenday.6911@gmail.com>
2018-04-03 10:39:31 +0530

    introduced RootQueryType

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = requires('express');
 const graphqlHTTP = requires('express-graphql');
+const schema = require('./schema/schema.js');
 
 const app = express();
 
 app.get('/graphql', graphqlHTTP({
-    
+    schema
 }));
 
 app.listen(4000,()=>{
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLSring } = graphql;
+const { GraphQLObjectType, GraphQLSring, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
     name : 'Book',
@@ -8,4 +8,22 @@ const BookType = new GraphQLObjectType({
         name : {type : GraphQLString},
         genre : {type : GraphQLSring}
     })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+    name : 'RootQueryType',
+    fields : {
+        book : {
+            type : BookType,
+            args : { id : {type : GraphQLSring} },
+            resolve(parent,args){
+              // code for fetching data from db
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query : RootQuery
 });
