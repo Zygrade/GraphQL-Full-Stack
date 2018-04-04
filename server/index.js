85f380135d306ea84712ccaf48d42ef8cb4e32c7
Zygrade <hsenday.6911@gmail.com>
2018-04-05 00:07:17 +0530

    errors rectified

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-const express = requires('express');
-const graphqlHTTP = requires('express-graphql');
-const schema = require('./schema/schema.js');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
-app.get('/graphql', graphqlHTTP({
-    schema
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql : true
 }));
 
 app.listen(4000,()=>{
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,14 +1,22 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLSring, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const _ = require('lodash');
 
 const BookType = new GraphQLObjectType({
     name : 'Book',
     fields : ()=>({
         id : {type : GraphQLString},
         name : {type : GraphQLString},
-        genre : {type : GraphQLSring}
+        genre : {type : GraphQLString}
     })
 });
+
+// dummy data
+const books = [
+    { name : 'Name of the wind', genre : 'Fantasy', id: '1' },
+    { name : 'The final Empire', genre : 'Fantasy', id: '2' },
+    { name : 'The long earth', genre : 'Sci-fi', id: '3' },
+];
 
 // Root Query
 const RootQuery = new GraphQLObjectType({
@@ -16,9 +24,10 @@ const RootQuery = new GraphQLObjectType({
     fields : {
         book : {
             type : BookType,
-            args : { id : {type : GraphQLSring} },
+            args : { id : {type : GraphQLString} },
             resolve(parent,args){
               // code for fetching data from db
+              return _.find(books,{id:args.id});
             }
         }
     }
